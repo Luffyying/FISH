98fb7ecd10f0b52166a5c05f722a1d3b9614f596
Luffyying <lufei_ying@sina.com>
2016-09-17 23:12:13 +0800

    use @media

---
+++ routine layout purely/header.php
@@ -0,0 +1,23 @@
+<header id='header'>
+	<div class='wrapper'>
+		<img src="img/logo.png" alt="">
+		<div id="header-menu">
+		  	<a href="#header-menu" id="btn-show" class="btn btn-show"><span class="glyphicon glyphicon-align-justify"></span></a>
+            <a href="#header-nav" id="btn-hide" class="btn btn-hide"><span class="glyphicon glyphicon-remove"></span></a>
+			<ul id="header-nav" class="header-nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Portfolio</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contact</a></li>
+				<li class="search">
+					<div id='search-box'>
+						<input type="search" id='search-input' 
+						placeholder="Enter your search content">
+						<button type="submit" id='search-btn'></button>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</header>
